8ecf2129b7bf5fe290ce5e9f479349bd73d7d0f7
davidLucas-BR <davidloliveiras@gmail.com>
2024-12-03 22:24:03 -0300

    Atualizando teste do Professor Felipe

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("vamos começar")
+let nomeHeroi = "Felipe"
+let xpHeroi = 1500
+let nivelHeroi = ""
+
+  if (xpHeroi <= 1000){
+    nivelHeroi = "Bronze"}
+  else if (xpHeroi > 1000 && xpHeroi <= 2000){
+    nivelHeroi = "Prata"}
+  
+  console.log("O Herói de nome " + nomeHeroi + " está de nível " + nivelHeroi)
+
+
