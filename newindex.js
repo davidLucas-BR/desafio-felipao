698f2412d70ddbb4a29475532df892604fdb836d
davidLucas-BR <davidloliveiras@gmail.com>
2024-12-26 11:19:21 -0300

    Desafio Calculadora de Partidas  Rankeadas do Felipe.DIO

--- newindex.js
+++ newindex.js
@@ -1,23 +1,32 @@
-let resultCalculator =  result(150, 15)
+let resultCalculator =  result(0, 0)
 let classification = ""
 
 function result(victory, defeat){
     sub = victory - defeat
     return sub}
 if (resultCalculator <= 10){
-    classification = "Ferro"}
+    classification = "Ferro"
+}
 else if (resultCalculator > 10 && resultCalculator <= 20){
-    classification = "Bronze"}
+    classification = "Bronze"
+}
 else if (resultCalculator > 20 && resultCalculator <= 50){
-    classification = "Prata"}
+    classification = "Prata"
+}
 else if (resultCalculator > 50 && resultCalculator <= 80    ){
-    classification = "Ouro"}
+    classification = "Ouro"
+}
 else if(resultCalculator > 80 && resultCalculator <= 90){
-    classification = "Diamante"}
+    classification = "Diamante"
+}
 else if(resultCalculator > 90 && resultCalculator <= 100){
-    classification = "Lendrário"}
+    classification = "Lendrário"
+}
 else if(resultCalculator > 100){
-    classification = "Imortal"}
+    classification = "Imortal"
+}
+default console.log("O Herói não jogou rankeadas!")
+
 
 console.log("O Herói tem de saldo de " + resultCalculator + " está no nível de " + classification)
 
