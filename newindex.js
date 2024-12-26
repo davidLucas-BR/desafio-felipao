56f659fe5470bba946b55595d97f5c6167dc99ed
davidLucas-BR <davidloliveiras@gmail.com>
2024-12-26 10:58:41 -0300

    Segundo desafio do Felipe.DIO

---
+++ newindex.js
@@ -0,0 +1,23 @@
+let resultCalculator =  result(150, 15)
+let classification = ""
+
+function result(victory, defeat){
+    sub = victory - defeat
+    return sub}
+if (resultCalculator <= 10){
+    classification = "Ferro"}
+else if (resultCalculator > 10 && resultCalculator <= 20){
+    classification = "Bronze"}
+else if (resultCalculator > 20 && resultCalculator <= 50){
+    classification = "Prata"}
+else if (resultCalculator > 50 && resultCalculator <= 80    ){
+    classification = "Ouro"}
+else if(resultCalculator > 80 && resultCalculator <= 90){
+    classification = "Diamante"}
+else if(resultCalculator > 90 && resultCalculator <= 100){
+    classification = "Lendrário"}
+else if(resultCalculator > 100){
+    classification = "Imortal"}
+
+console.log("O Herói tem de saldo de " + resultCalculator + " está no nível de " + classification)
+
